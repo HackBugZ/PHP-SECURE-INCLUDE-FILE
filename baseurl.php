82c50a301deda5cb66c732f39c1b9350e0650f5a
HackBugZ <94540527+HackBugZ@users.noreply.github.com>
2022-01-16 11:19:06 +0100

    Create baseurl.php

---
+++ baseurl.php
@@ -0,0 +1,13 @@
+<pre>
+<?php
+if((!defined("z"))){exit;}	
+
+print_r(__DIR__);
+print_r('<hr>');
+print_r(__FILE__);
+print_r('<hr>');
+print_r($SecPATHARR);
+print_r('<hr>');
+print_r($LoadPATH);
+	
+?>
